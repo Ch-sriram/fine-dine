546b03d97265a0d1f219fc56de5c869866f3bec7
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-04-14 00:06:35 +0530

    Added animation effects using waypoint (jQuery) & animate.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-$(document).ready(function() {
-    /**
-     * Selecting the way-points
-     */
-
+$(document).ready(function () {
+    
+/* For sticky navigation */
     $('.section-features').waypoint(function (direction) {
         if (direction == "down")
             $('nav').addClass('sticky');
@@ -10,4 +8,45 @@ $(document).ready(function() {
     }, {
         offset: '60px;'
     }); 
+
+/* Scroll on clicking the respective buttons */
+    // $('.js--scroll-to-plans').click(function() {
+    //     $('html, body').animate({
+    //         scrollTop: $('.section-plans').offset().top
+    //     }, 1000);
+    // });
+    
+    // $('.js--scroll-to-start').click(function() {
+    //     $('html, body').animate({
+    //         scrollTop: $('.section-features').offset().top
+    //     }, 1000);
+    // });
+
+/* Animations on scroll */
+    // for .section-features
+    $('.js--wp-1').waypoint(function() {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+       offset: '50%' 
+    });
+    
+    // for 
+    $('.js--wp-2').waypoint(function() {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+       offset: '50%' 
+    });
+    
+    $('.js--wp-3').waypoint(function () {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+       offset: '50%' 
+    });
+    
+    $('.js--wp-4').waypoint(function () {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+       offset: '50%' 
+    });
+
 });
